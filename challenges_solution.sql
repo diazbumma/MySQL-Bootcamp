21f95c328336a755565a8098572ad4f105c5790b
Odiaz Bumma <odiazbumma@gmail.com>
2020-04-13 13:13:27 +0700

    Added table constraints exercise solution

--- challenges_solution.sql
+++ challenges_solution.sql
@@ -2,3 +2,13 @@
 CREATE TABLE pastries (name VARCHAR(50), quantity INT);
 SHOW COLUMNS FROM pastries;
 DROP TABLE pastries;
+
+/* TABLE CONSTRAINTS */
+CREATE TABLE employees (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  last_name VARCHAR(100) NOT NULL,
+  first_name VARCHAR(100) NOT NULL,
+  middle_name VARCHAR(100),
+  age INT NOT NULL,
+  current_status VARCHAR(20) NOT NULL DEFAULT 'employed'
+);
