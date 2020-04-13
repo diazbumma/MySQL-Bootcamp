919c9461c1c8be7d9774c18b3ce778569f59bac3
Odiaz Bumma <odiazbumma@gmail.com>
2020-04-13 13:47:53 +0700

    Added select challenges solutions

--- challenges_solution.sql
+++ challenges_solution.sql
@@ -12,3 +12,9 @@ CREATE TABLE employees (
   age INT NOT NULL,
   current_status VARCHAR(20) NOT NULL DEFAULT 'employed'
 );
+
+/* SELECT CHALLENGES */
+SELECT cat_id FROM cats;
+SELECT name, breed FROM cats;
+SELECT name, age FROM cats WHERE breed = 'Tabby';
+SELECT cat_id, age FROM cats WHERE cat_id = age;
