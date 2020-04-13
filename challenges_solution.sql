cf8faddeddd1189539071bab0068e3f4e4b89725
Odiaz Bumma <odiazbumma@gmail.com>
2020-04-13 13:54:19 +0700

    Added update challenges solutions

--- challenges_solution.sql
+++ challenges_solution.sql
@@ -18,3 +18,8 @@ SELECT cat_id FROM cats;
 SELECT name, breed FROM cats;
 SELECT name, age FROM cats WHERE breed = 'Tabby';
 SELECT cat_id, age FROM cats WHERE cat_id = age;
+
+/* UPDATE CHALLENGES */
+UPDATE cats SET name = 'Jack' WHERE name = 'Jackson';
+UPDATE cats SET breed = 'British Shorthair' WHERE name = 'Ringo';
+UPDATE cats SET age =  12 WHERE breed = 'Maine Coon';
