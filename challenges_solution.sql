a77d223f2d697ebb5c4c6ecdd43015f356c5c17c
Odiaz Bumma <odiazbumma@gmail.com>
2020-04-14 15:02:22 +0700

    Added String function challenges solutions

--- challenges_solution.sql
+++ challenges_solution.sql
@@ -70,3 +70,16 @@ DELETE FROM shirts WHERE last_worn = 200;
 DELETE FROM shirts WHERE article = 'tank top';
 DELETE FROM shirts;
 DROP TABLE shirts;
+
+/* STRING FUNCTION CHALLENGES */
+SELECT REVERSE(UPPER('Why does my cat look at me with such hatred?'));
+SELECT REPLACE(title, ' ', '->') AS title FROM books;
+SELECT author_lname AS forwards, REVERSE(author_lname) AS backwards FROM books;
+SELECT UPPER(CONCAT(author_fname, ' ', author_lname)) AS 'full name in caps' FROM books;
+SELECT CONCAT(title, ' was released in ', released_year) AS blurb FROM books;
+SELECT title, CHAR_LENGTH(title) AS 'character count' FROM books;
+SELECT
+  CONCAT(SUBSTR(title, 1, 10), '...') AS 'short title',
+  CONCAT(author_lname, ',', author_fname) AS author,
+  CONCAT(stock_quantity, ' in stock') AS quantity
+FROM books;
