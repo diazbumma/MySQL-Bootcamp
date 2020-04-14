ce17cbe5e16fe6d5e705c5ccd6cb792e128b6737
Odiaz Bumma <odiazbumma@gmail.com>
2020-04-14 21:32:46 +0700

    Added refining selections exercises solutions

--- challenges_solution.sql
+++ challenges_solution.sql
@@ -83,3 +83,20 @@ SELECT
   CONCAT(author_lname, ',', author_fname) AS author,
   CONCAT(stock_quantity, ' in stock') AS quantity
 FROM books;
+
+/* REFINING SELECTIONS EXERCISES */
+SELECT title FROM books WHERE title LIKE '%stories%';
+SELECT title, pages FROM books ORDER BY pages DESC LIMIT 1;
+SELECT CONCAT(title, ' - ', released_year) AS summary FROM books ORDER BY released_year DESC LIMIT 3;
+SELECT title, author_lname FROM books WHERE author_lname LIKE '% %';
+SELECT title, released_year, stock_quantity FROM books ORDER BY stock_quantity, title LIMIT 3;
+SELECT title, author_lname FROM books ORDER BY author_lname, title;
+SELECT UPPER(
+  CONCAT(
+    'MY FAVORITE AUTHOR IS ',
+    author_fname,
+    ' ',
+    author_lname,
+    '!'
+  )
+) AS yell FROM books ORDER BY author_lname;
