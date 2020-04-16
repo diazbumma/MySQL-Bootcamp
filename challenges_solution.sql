ed4a2bd2b976beed4a04e9fb4cb77d97cc11cc53
Odiaz Bumma <odiazbumma@gmail.com>
2020-04-16 16:52:20 +0700

    Added Logical operator exercises solutions

--- challenges_solution.sql
+++ challenges_solution.sql
@@ -122,3 +122,23 @@ CREATE TABLE tweets (
   username VARCHAR(255),
   created_at TIMESTAMP DEFAULT NOW()
 );
+
+/* LOGICAL OPERATOR EXERCISES SOLUTIONS */
+SELECT * FROM books WHERE released_year < 1980;
+SELECT * FROM books WHERE author_lname = 'Eggers' OR author_lname = 'Chabon';
+SELECT * FROM books WHERE author_lname = 'Lahiri' AND released_year > 2000;
+SELECT * FROM books WHERE pages BETWEEN 100 AND 200;
+SELECT * FROM books WHERE author_lname LIKE 'C%' OR author_lname LIKE 'S%';
+SELECT title, author_lname,
+  CASE
+    WHEN title LIKE '%stories%' THEN 'Short Stories'
+    WHEN title LIKE '%Just Kids%' OR title LIKE '%A Heartbreaking Work%' THEN 'Memoir'
+    ELSE 'Novel'
+  END AS TYPES
+FROM books;
+SELECT title, author_lname,
+  CASE
+    WHEN COUNT(*) > 1 THEN CONCAT(COUNT(*), ' books')
+    ELSE '1 book'
+  END AS 'COUNT'
+FROM books GROUP BY author_fname, author_lname ORDER BY author_lname;
