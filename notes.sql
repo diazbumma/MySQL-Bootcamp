abf1e1c88ee93a7f48156f74765145e3be103973
Odiaz Bumma <odiazbumma@gmail.com>
2020-04-24 05:25:30 +0700

    Added note about primary keys

--- notes.sql
+++ notes.sql
@@ -42,3 +42,11 @@ CREATE TABLE orders (
   FOREIGN KEY(customer_id) REFERENCES customers(id)   --assigns customer_id as references of id in customers table
   ON DELETE CASCADE                                   --deletes record when the foreign record deleted
 );
+
+CREATE TABLE photo_tags (
+  photo_id INT NOT NULL,
+  tag_id INT NOT NULL,
+  FOREIGN KEY (photo_id) REFERENCES photos(id) ON DELETE CASCADE,
+  FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE,
+  PRIMARY KEY (photo_id, tag_id)                      --make sure combinations of two id won't be repeated
+);
