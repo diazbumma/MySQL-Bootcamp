41d92deecdc3327443a3878a655022f7396e2bad
Odiaz Bumma <odiazbumma@gmail.com>
2020-04-27 14:34:09 +0700

    Added 500 users exercise solutions

--- challenges_solution.sql
+++ challenges_solution.sql
@@ -242,3 +242,27 @@ INNER JOIN reviews
 INNER JOIN reviewers
   ON reviews.reviewer_id = reviewers.id
 ORDER BY title;
+
+/* 500 users exercises*/
+SELECT DATE_FORMAT(created_at, '%M %D %Y') AS earliest_date FROM users
+  ORDER BY created_at LIMIT 1;
+
+SELECT * FROM users WHERE created_at IN (SELECT MIN(created_at) FROM users);
+
+SELECT MONTHNAME(created_at) AS month, COUNT(*) AS count FROM users
+  GROUP BY month
+  ORDER BY count DESC;
+
+SELECT COUNT(*) AS yahoo_users FROM users WHERE email LIKE '%@yahoo.com';
+
+SELECT
+    CASE
+      WHEN email LIKE '%@gmail.com' THEN 'gmail'
+      WHEN email LIKE '%@yahoo.com' THEN 'yahoo'
+      WHEN email LIKE '%@hotmail.com' THEN 'hotmail'
+      ELSE 'other'
+    END AS provider,
+    COUNT(*) AS total_users
+  FROM users
+  GROUP BY provider
+  ORDER BY total_users DESC;
